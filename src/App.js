4010fca85de5519f740368c29194a735f9b371c3
Ravinder Singh <ravi_29_12@hotmail.com>
2018-07-16 15:01:53 +0100

    Added GMaps and Sidebar component.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
+import GMaps from './components/GMaps';
+import Sidebar from './components/Sidebar';
 import './App.css';
 
 class App extends Component {
     render() {
-        return <div className="App" />;
+        return (
+            <div className="App">
+                <Sidebar />
+                <GMaps />
+            </div>
+        );
     }
 }
 
