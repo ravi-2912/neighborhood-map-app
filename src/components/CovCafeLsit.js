1f5ccd7aac0223a096cf7dea4afe64c401d7e3c2
Ravinder Singh <ravi_29_12@hotmail.com>
2018-07-18 00:00:03 +0100

    Added Cafe list.

---
+++ src/components/CovCafeLsit.js
@@ -0,0 +1,383 @@
+export const CovCafeList = {
+    meta: {
+        code: 200,
+        requestId: '5b4e5ea41ed219421d1fbb7c'
+    },
+    response: {
+        venues: [
+            {
+                id: '554e17c5498e79a568f29760',
+                name: 'Rising Cafe',
+                location: {
+                    address: 'Priory St',
+                    lat: 52.40926334458678,
+                    lng: -1.5073460552133593,
+                    labeledLatLngs: [
+                        {
+                            label: 'display',
+                            lat: 52.40926334458678,
+                            lng: -1.5073460552133593
+                        }
+                    ],
+                    distance: 108,
+                    postalCode: 'CV1 5FB',
+                    cc: 'GB',
+                    city: 'Coventry',
+                    state: 'Coventry',
+                    country: 'United Kingdom',
+                    formattedAddress: ['Priory St', 'Coventry', 'CV1 5FB', 'United Kingdom']
+                },
+                categories: [
+                    {
+                        id: '4bf58dd8d48988d16d941735',
+                        name: 'Café',
+                        pluralName: 'Cafés',
+                        shortName: 'Café',
+                        icon: {
+                            prefix: 'https://ss3.4sqi.net/img/categories_v2/food/cafe_',
+                            suffix: '.png'
+                        },
+                        primary: true
+                    }
+                ],
+                referralId: 'v-1531862692',
+                hasPerk: false
+            },
+            {
+                id: '524e8401498e3beb830f3e5f',
+                name: 'M&S Café',
+                location: {
+                    lat: 52.4080004531604,
+                    lng: -1.5125420555231128,
+                    labeledLatLngs: [
+                        {
+                            label: 'display',
+                            lat: 52.4080004531604,
+                            lng: -1.5125420555231128
+                        }
+                    ],
+                    distance: 371,
+                    cc: 'GB',
+                    country: 'United Kingdom',
+                    formattedAddress: ['United Kingdom']
+                },
+                categories: [
+                    {
+                        id: '4bf58dd8d48988d16d941735',
+                        name: 'Café',
+                        pluralName: 'Cafés',
+                        shortName: 'Café',
+                        icon: {
+                            prefix: 'https://ss3.4sqi.net/img/categories_v2/food/cafe_',
+                            suffix: '.png'
+                        },
+                        primary: true
+                    }
+                ],
+                referralId: 'v-1531862692',
+                hasPerk: false
+            },
+            {
+                id: '4c62969379d1e21ec85ed615',
+                name: 'Chapters Cafe',
+                location: {
+                    address: 'Coventry University',
+                    lat: 52.40620362255668,
+                    lng: -1.5019066724031103,
+                    labeledLatLngs: [
+                        {
+                            label: 'display',
+                            lat: 52.40620362255668,
+                            lng: -1.5019066724031103
+                        }
+                    ],
+                    distance: 422,
+                    cc: 'GB',
+                    city: 'West Midlands',
+                    state: 'West Midlands',
+                    country: 'United Kingdom',
+                    formattedAddress: ['Coventry University', 'West Midlands', 'United Kingdom']
+                },
+                categories: [
+                    {
+                        id: '4bf58dd8d48988d1a1941735',
+                        name: 'College Cafeteria',
+                        pluralName: 'College Cafeterias',
+                        shortName: 'Cafeteria',
+                        icon: {
+                            prefix: 'https://ss3.4sqi.net/img/categories_v2/education/cafeteria_',
+                            suffix: '.png'
+                        },
+                        primary: true
+                    }
+                ],
+                referralId: 'v-1531862692',
+                hasPerk: false
+            },
+            {
+                id: '4bf80c064a67c928309e24cf',
+                name: 'Pumpkin Cafe',
+                location: {
+                    address: 'Coventry Railway Station',
+                    lat: 52.400987070048444,
+                    lng: -1.5139044033340343,
+                    labeledLatLngs: [
+                        {
+                            label: 'display',
+                            lat: 52.400987070048444,
+                            lng: -1.5139044033340343
+                        }
+                    ],
+                    distance: 936,
+                    postalCode: 'CV1 2GT',
+                    cc: 'GB',
+                    city: 'West Midlands',
+                    state: 'West Midlands',
+                    country: 'United Kingdom',
+                    formattedAddress: ['Coventry Railway Station', 'West Midlands', 'CV1 2GT', 'United Kingdom']
+                },
+                categories: [
+                    {
+                        id: '4bf58dd8d48988d16d941735',
+                        name: 'Café',
+                        pluralName: 'Cafés',
+                        shortName: 'Café',
+                        icon: {
+                            prefix: 'https://ss3.4sqi.net/img/categories_v2/food/cafe_',
+                            suffix: '.png'
+                        },
+                        primary: true
+                    }
+                ],
+                referralId: 'v-1531862692',
+                hasPerk: false
+            },
+            {
+                id: '5739bef9498e555304d27249',
+                name: 'PANWPS Café at Stafford House',
+                location: {
+                    lat: 52.419667,
+                    lng: -1.503104,
+                    labeledLatLngs: [
+                        {
+                            label: 'display',
+                            lat: 52.419667,
+                            lng: -1.503104
+                        }
+                    ],
+                    distance: 1294,
+                    cc: 'GB',
+                    city: 'Coventry',
+                    state: 'Coventry',
+                    country: 'United Kingdom',
+                    formattedAddress: ['Coventry', 'United Kingdom']
+                },
+                categories: [
+                    {
+                        id: '4bf58dd8d48988d16d941735',
+                        name: 'Café',
+                        pluralName: 'Cafés',
+                        shortName: 'Café',
+                        icon: {
+                            prefix: 'https://ss3.4sqi.net/img/categories_v2/food/cafe_',
+                            suffix: '.png'
+                        },
+                        primary: true
+                    }
+                ],
+                referralId: 'v-1531862692',
+                hasPerk: false
+            },
+            {
+                id: '51656841e4b0ef70e65ec9a7',
+                name: 'Café Rouge',
+                location: {
+                    address: '2 Belgrade Plaza',
+                    lat: 52.410262441431584,
+                    lng: -1.5143672096840228,
+                    labeledLatLngs: [
+                        {
+                            label: 'display',
+                            lat: 52.410262441431584,
+                            lng: -1.5143672096840228
+                        }
+                    ],
+                    distance: 539,
+                    postalCode: 'CV1 4AJ',
+                    cc: 'GB',
+                    city: 'Coventry',
+                    state: 'Coventry',
+                    country: 'United Kingdom',
+                    formattedAddress: ['2 Belgrade Plaza', 'Coventry', 'CV1 4AJ', 'United Kingdom']
+                },
+                categories: [
+                    {
+                        id: '4bf58dd8d48988d10c941735',
+                        name: 'French Restaurant',
+                        pluralName: 'French Restaurants',
+                        shortName: 'French',
+                        icon: {
+                            prefix: 'https://ss3.4sqi.net/img/categories_v2/food/french_',
+                            suffix: '.png'
+                        },
+                        primary: true
+                    }
+                ],
+                referralId: 'v-1531862692',
+                hasPerk: false
+            },
+            {
+                id: '4ed0f01c0e01f1a88068dca6',
+                name: 'The Rose Bud Cafe',
+                location: {
+                    lat: 52.410270347071574,
+                    lng: -1.4858436163368411,
+                    labeledLatLngs: [
+                        {
+                            label: 'display',
+                            lat: 52.410270347071574,
+                            lng: -1.4858436163368411
+                        }
+                    ],
+                    distance: 1460,
+                    cc: 'GB',
+                    country: 'United Kingdom',
+                    formattedAddress: ['United Kingdom']
+                },
+                categories: [
+                    {
+                        id: '4bf58dd8d48988d16d941735',
+                        name: 'Café',
+                        pluralName: 'Cafés',
+                        shortName: 'Café',
+                        icon: {
+                            prefix: 'https://ss3.4sqi.net/img/categories_v2/food/cafe_',
+                            suffix: '.png'
+                        },
+                        primary: true
+                    }
+                ],
+                referralId: 'v-1531862692',
+                hasPerk: false
+            },
+            {
+                id: '4e342757aeb72268626436f9',
+                name: 'Café Italia',
+                location: {
+                    address: '9-11 Trinity St',
+                    lat: 52.40963848720137,
+                    lng: -1.5094213170819153,
+                    labeledLatLngs: [
+                        {
+                            label: 'display',
+                            lat: 52.40963848720137,
+                            lng: -1.5094213170819153
+                        }
+                    ],
+                    distance: 216,
+                    postalCode: 'CV1 1FJ',
+                    cc: 'GB',
+                    city: 'West Midlands',
+                    state: 'West Midlands',
+                    country: 'United Kingdom',
+                    formattedAddress: ['9-11 Trinity St', 'West Midlands', 'CV1 1FJ', 'United Kingdom']
+                },
+                categories: [
+                    {
+                        id: '4bf58dd8d48988d16d941735',
+                        name: 'Café',
+                        pluralName: 'Cafés',
+                        shortName: 'Café',
+                        icon: {
+                            prefix: 'https://ss3.4sqi.net/img/categories_v2/food/cafe_',
+                            suffix: '.png'
+                        },
+                        primary: true
+                    }
+                ],
+                referralId: 'v-1531862692',
+                hasPerk: false
+            },
+            {
+                id: '4d32fb8df8c9224b7739b6d2',
+                name: 'Café Oregano',
+                location: {
+                    address: '13 High Street',
+                    lat: 52.4075,
+                    lng: -1.509278,
+                    labeledLatLngs: [
+                        {
+                            label: 'display',
+                            lat: 52.4075,
+                            lng: -1.509278
+                        }
+                    ],
+                    distance: 172,
+                    postalCode: 'CV1 5RE',
+                    cc: 'GB',
+                    city: 'Coventry',
+                    state: 'Coventry',
+                    country: 'United Kingdom',
+                    formattedAddress: ['13 High Street', 'Coventry', 'CV1 5RE', 'United Kingdom']
+                },
+                categories: [
+                    {
+                        id: '4bf58dd8d48988d16d941735',
+                        name: 'Café',
+                        pluralName: 'Cafés',
+                        shortName: 'Café',
+                        icon: {
+                            prefix: 'https://ss3.4sqi.net/img/categories_v2/food/cafe_',
+                            suffix: '.png'
+                        },
+                        primary: true
+                    }
+                ],
+                referralId: 'v-1531862692',
+                hasPerk: false
+            },
+            {
+                id: '4d5bceabfb186dcb58daf79a',
+                name: 'Eat Cafe',
+                location: {
+                    address: 'Coventry University - GE Building',
+                    crossStreet: 'Priory Street',
+                    lat: 52.408349,
+                    lng: -1.50886,
+                    labeledLatLngs: [
+                        {
+                            label: 'display',
+                            lat: 52.408349,
+                            lng: -1.50886
+                        }
+                    ],
+                    distance: 119,
+                    cc: 'GB',
+                    city: 'Coventry',
+                    state: 'Coventry',
+                    country: 'United Kingdom',
+                    formattedAddress: [
+                        'Coventry University - GE Building (Priory Street)',
+                        'Coventry',
+                        'United Kingdom'
+                    ]
+                },
+                categories: [
+                    {
+                        id: '4bf58dd8d48988d16d941735',
+                        name: 'Café',
+                        pluralName: 'Cafés',
+                        shortName: 'Café',
+                        icon: {
+                            prefix: 'https://ss3.4sqi.net/img/categories_v2/food/cafe_',
+                            suffix: '.png'
+                        },
+                        primary: true
+                    }
+                ],
+                referralId: 'v-1531862692',
+                hasPerk: false
+            }
+        ]
+    }
+};
