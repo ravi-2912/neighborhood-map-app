861d2b2ac8686f2da1e18864edd3dba733675227
Ravinder Singh <ravi_29_12@hotmail.com>
2018-07-19 00:40:26 +0100

    Serch API function update.

--- src/FourSquareAPI.js
+++ src/FourSquareAPI.js
@@ -2,15 +2,17 @@ var FOURSQUARE_CLIENT_ID = 'OJWWM4KM2QIC1ZYNR2GGNPSYVMFIIMQQJSYCHQJ1DVOQHEIO';
 var FOURSQUARE_CLIENT_SECRET = '5X0YJTIWGJC34YIDAAIOAEXXFBB2SHE3JPWNYO23ROQKYMX2';
 
 export const search = (query, location) =>
-    fetch(`https://api.foursquare.com/v2/venues/search?
-            ll=${location.lat},${location.lng}
-            &query=${query}
-            &limit=10
-            &radius=50000
-            &client_id=${FOURSQUARE_CLIENT_ID}
-            &client_secret=${FOURSQUARE_CLIENT_SECRET}
-            &v=20180717`)
-        .then(res => res.json())
+    fetch(
+        `https://api.foursquare.com/v2/venues/search?` +
+            `ll=${location.lat},${location.lng}` +
+            `&query=${query}` +
+            `&limit=10` +
+            `&radius=50000` +
+            `&client_id=${FOURSQUARE_CLIENT_ID}` +
+            `&client_secret=${FOURSQUARE_CLIENT_SECRET}` +
+            `&v=20180717`
+    )
+        .then(handleErrors)
         .catch(err => console.log('Foursquare cannot be reached\n', err));
 
 export const venue = venueID => {
@@ -18,4 +20,12 @@ export const venue = venueID => {
         .then(res => (res.status === 200 ? res.json() : (res.infowindow.msg = 'Sorry data cannot be loaded')))
         .then(data => data.response.venues)
         .catch(err => console.log('Foursquare cannot be reached\n', err));
+};
+
+const handleErrors = response => {
+    console.log(response);
+    if (!response.ok) {
+        throw Error('FourSqaure response error');
+    }
+    return response.json();
 };
